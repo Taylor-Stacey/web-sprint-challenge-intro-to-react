c870db65fa73b06b7b5f2b4b6d864c80de6de58c
Taylor <Taylor88stacey@gmail.com>
2022-03-25 13:19:01 -0700

    not sure what to do

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import axios from 'axios';
+import { baseURL } from './constants';
+import HeroCard from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get(`${baseURL}`)
+    .then(res => {
+      setData(res.data)
+      console.log(data);
+    })
+    
+    .catch(err => console.error(err))
+  },[])
+
+      
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -11,6 +28,8 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+        <HeroCard data={data} />
+        
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,21 @@
 // Write your Character component here
+import React from "react";
+
+
+
+
+const HeroCard = (props) => {
+    return (
+       <h3>{props.name}</h3>
+    )
+};
+
+export default HeroCard;
+
+// dummy info
+// {name: 'Luke Skywalker', height: '172', mass: '77', hair_color: 'blond', skin_color: 'fair', …}
+// 1: {name: 'C-3PO', height: '167', mass: '75', hair_color: 'n/a', skin_color: 'gold', …}
+// 2: {name: 'R2-D2', height: '96', mass: '32', hair_color: 'n/a', skin_color: 'white, blue', …}
+// 3: {name: 'Darth Vader', height: '202', mass: '136', hair_color: 'none', skin_color: 'white', …}
+// 4: {name: 'Leia Organa', height: '150', mass: '49', hair_color: 'brown', skin_color: 'light', …}
+// 5: {name: 'Owen Lars', height: '178', mass: '120', hair_color: 'brown, grey', skin_color: 'light', …}
