d219155fa055280ce04d05694f4e2edd208312d8
Taylor <Taylor88stacey@gmail.com>
2022-03-26 12:47:50 -0700

    still needs styled

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,21 +1,69 @@
 // Write your Character component here
-import React from "react";
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
 
 
+const Character = ({props}) => {
+    const {
+        name,
+        birth_year,
+        gender,
+        height,
+        mass,
+        eye_color,
+    } = props;
+
+    const StyledDiv = styled.div`
+    
+    `
+    const StyledH2 = styled.h2`
+    
+    `
+    const StyledP = styled.p`
+    
+    `
 
 
-const HeroCard = (props) => {
     return (
-       <h3>{props.name}</h3>
+        <StyledDiv>
+            <StyledH2>
+                {name}
+            </StyledH2>
+            <StyledP>
+               Birth year: {birth_year}
+            </StyledP>
+            <StyledP>
+               Gender: {gender}
+            </StyledP>
+            <StyledP>
+               Height: {height}
+            </StyledP>
+            <StyledP>
+               Mass: {mass}
+            </StyledP>
+            <StyledP>
+               Eye Color: {eye_color}
+            </StyledP>
+        </StyledDiv>
     )
-};
+}
 
-export default HeroCard;
+export default Character;
 
-// dummy info
-// {name: 'Luke Skywalker', height: '172', mass: '77', hair_color: 'blond', skin_color: 'fair', …}
-// 1: {name: 'C-3PO', height: '167', mass: '75', hair_color: 'n/a', skin_color: 'gold', …}
-// 2: {name: 'R2-D2', height: '96', mass: '32', hair_color: 'n/a', skin_color: 'white, blue', …}
-// 3: {name: 'Darth Vader', height: '202', mass: '136', hair_color: 'none', skin_color: 'white', …}
-// 4: {name: 'Leia Organa', height: '150', mass: '49', hair_color: 'brown', skin_color: 'light', …}
-// 5: {name: 'Owen Lars', height: '178', mass: '120', hair_color: 'brown, grey', skin_color: 'light', …}
+
+// birth_year: "19BBY"
+// created: "2014-12-09T13:50:51.644000Z"
+// edited: "2014-12-20T21:17:56.891000Z"
+// eye_color: "blue"
+// films: (4) ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi', 'Revenge of the Sith']
+// gender: "male"
+// hair_color: "blond"
+// height: "172"
+// homeworld: "http://swapi.dev/api/planets/1/"
+// mass: "77"
+// name: "Luke Skywalker"
+// skin_color: "fair"
+// species: []
+// starships: (2) ['http://swapi.dev/api/starships/12/', 'http://swapi.dev/api/starships/22/']
+// url: "http://swapi.dev/api/people/1/"
+// vehicles: (2) ['http://swapi.dev/api/vehicles/14/', 'http://swapi.dev/api/vehicles/30/']
